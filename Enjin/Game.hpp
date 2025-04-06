c96d40d96f8ee3de24a746c9b1798d1f5e5aa90e
SebastienMorris <sebw.morris@gmail.com>
2025-04-06 16:58:40 +0200

    clear game

--- Enjin/Game.hpp
+++ Enjin/Game.hpp
@@ -2,8 +2,6 @@
 
 #include <vector>
 
-#include "Enemy.h"
-#include "Entity.h"
 #include "SFML/Graphics.hpp"
 #include "SFML/System.hpp"
 #include "SFML/Window.hpp"
@@ -13,32 +11,16 @@
 #include "Particle.hpp"
 #include "ParticleMan.hpp"
 
-#include "Player.h"
-
-#include "SecondOrderDynamics.h"
 
 using namespace sf;
 
 class HotReloadShader;
 class Entity;
 class Game {
-
-private:
-	float camFrequency = 1.0f;
-	float camDampening = 0.9f;
-	float camBouciness = 0.0f;
-
-	float camShakeDuration = 0.5f;
-	float camShakeFrequency = 4.0f;
-	float camShakeAmplitude = 4.0f;
-
-	SecondOrderDynamics secondOrderDynamics = SecondOrderDynamics(camFrequency, camDampening, camBouciness, {0, 0, 0});
 	
 public:
 	
 	sf::RenderWindow*				win = nullptr;
-	const sf::View*					editView = nullptr;
-	sf::View*					    playView = nullptr;
 
 	sf::RectangleShape				bg;
 	HotReloadShader *				bgShader = nullptr;
@@ -46,58 +28,22 @@ public:
 	sf::Texture						tex;
 
 	bool							closing = false;
-	
-	std::vector<sf::Vector2i>		walls;
-	std::vector<sf::RectangleShape> wallSprites;
-	std::vector<Entity*> entities;
-	std::vector<Enemy*> enemies;
 
-	Player* player;
 
 	ParticleMan beforeParts;
 	ParticleMan afterParts;
-
-	bool editMode = true;
-	bool placeWallEnemytoggle = false;
-	bool eraseMode = false;
-	
-	bool mouseRight = false;
-
-	int currentMouseX = -1;
-	int currentMouseY = -1;
 
 	static Game* me;
 
 	Game(sf::RenderWindow * win);
 	~Game();
 
-	void cacheWalls();
-
 	void processInput(sf::Event ev);
-	bool wasPressedSpace = false;
 	void pollInput(double dt);
-	void onSpacePressed();
 
 	void update(double dt);
 
 	void draw(sf::RenderWindow& win);
-
-	bool isWall(int cx, int cy);
+	
 	void im();
-
-	void CreatePlayer(int spawnX, int spawnY);
-	void CreateEnemy(int spawnX, int spawnY);
-
-	bool hasCollision(float gx, float gy);
-	bool hasCollision(float gx, float gy, int height, Entity& self);
-	Enemy* hasCollisionEnemy(float gx, float gy);
-
-	void CamShake();
-
-	void EplaceWall(int mouseX, int mouseY);
-	void EplaceEnemy(int mouseX, int mouseY);
-	bool ECheckEntity(int gx, int gy, int xRange, int yRange, int height);
-
-	void Save(std::string filename);
-	void Load(std::string filename);
 };
